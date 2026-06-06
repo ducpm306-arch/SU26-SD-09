1d9570ad674edfae170a5ec31f1e60954070b844
DELL <minhduc.59h3@gmail.com>
2026-06-06 22:44:17 +0700

    Sửa security config

--- src/main/java/su26sd09/su26sd09/config/SecurityConfig.java
+++ src/main/java/su26sd09/su26sd09/config/SecurityConfig.java
@@ -28,15 +28,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authenticationProvider(authenticationProvider())
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/Login", "/api/auth/**","/verify-email","/home/**",
-//                                "/loai-phong", "/loai-phong/**",
-//                                "/phong", "/phong/**",
-//                                "/static/**", "/css/**", "/js/**", "/images/**",
-//                                "/*.css", "/*.js", "/*.jpg", "/*.png","/Register").permitAll()
-//                        .anyRequest().authenticated()
-                                .anyRequest().permitAll()
+                        .requestMatchers("/Login", "/api/auth/**","/verify-email","/home/**",
+                                "/loai-phong", "/loai-phong/**",
+                                "/phong", "/phong/**",
+                                "/static/**", "/css/**", "/js/**", "/images/**",
+                                "/*.css", "/*.js", "/*.jpg", "/*.png","/Register").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
                         .loginPage("/Login").
